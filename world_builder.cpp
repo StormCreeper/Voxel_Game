3b3077b23964437c705ac1ef8d962b70c96310f9
Telo <telo.philippe@telecom-paris.fr>
2024-05-17 02:55:07 +0200

    Reduced a bit the number of calls to noise

--- world_builder.cpp
+++ world_builder.cpp
@@ -3,16 +3,16 @@
 SimplexNoise WorldBuilder::sn{0.005f, 1.0f};
 
 uint8_t WorldBuilder::generation_function(glm::ivec3 world_pos) {
-    float mountain_val = abs(sn.fractal(8, world_pos.x, world_pos.z)) * 2.0f - 1.0f;
-    float plain_val = sn.fractal(4, world_pos.x * 0.4, world_pos.z * 0.4);
-    float sea_val = sn.fractal(8, world_pos.x * 0.2, world_pos.z * 0.2);
-
     float L = 1;
     float k = 18;
     float x0 = 0;
     float base_h = 0.2;
 
+    float sea_val = sn.fractal(2, world_pos.x * 0.2, world_pos.z * 0.2);
     sea_val = L / (1 + exp(-k * (sea_val - x0))) * (1 - base_h) + base_h;
+
+    float mountain_val = abs(sn.fractal(6, world_pos.x, world_pos.z)) * 2.0f - 1.0f;
+    float plain_val = sn.fractal(3, world_pos.x * 0.4, world_pos.z * 0.4);
 
     float lerp = sn.fractal(8, world_pos.x * 0.3, world_pos.z * 0.3) * 0.5f + 0.5f;
 
